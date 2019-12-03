7622be8e4eab6aa6d9828ed2950eab264717021c
Henryabrunning <55792186+Henryabrunning@users.noreply.github.com>
2019-12-03 14:16:53 +0000

    Added game files

---
+++ CT4005-Assesment2/Assets/Scripts/Player1Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Movement : MonoBehaviour
+{
+   public float speed = 12f;
+    public CharacterController controller;
+
+  
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
---
+++ CT4005-Assesment2/Assets/Scripts/Player2Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Movement : MonoBehaviour
+{
+    public float speed = 12f;
+    public CharacterController controller;
+
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal2");
+        float z = Input.GetAxis("Vertical2");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
