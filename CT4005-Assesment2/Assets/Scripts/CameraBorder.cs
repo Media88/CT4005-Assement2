c6f7be7077ee5f65a29596b7937931c3b3a9b1c8
Henry Brunning <55792186+Henryabrunning@users.noreply.github.com>
2019-12-10 14:01:56 +0000

    Added Player controllers
    Added players three and four, as well as controllers for all.
    Organised camera layout.

---
+++ CT4005-Assesment2/Assets/Scripts/CameraBorder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBorder : MonoBehaviour
+{
+
+    public BoxCollider boundBox;
+
+    private Vector3 minBounds;
+    private Vector3 maxBounds;
+
+    void Start()
+    {
+        minBounds = boundBox.bounds.min;
+        maxBounds = boundBox.bounds.max;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
--- CT4005-Assesment2/Assets/Scripts/Player1Movement.cs
+++ CT4005-Assesment2/Assets/Scripts/Player1Movement.cs
@@ -7,11 +7,14 @@ public class Player1Movement : MonoBehaviour
    public float speed = 12f;
     public CharacterController controller;
 
+    [SerializeField]
+    private string controllerPrefix = "";
+
   
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        float x = Input.GetAxis(controllerPrefix + "Horizontal");
+        float z = Input.GetAxis(controllerPrefix + "Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
