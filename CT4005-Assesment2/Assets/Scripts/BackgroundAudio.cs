aa11a251f7eb870e446016d7bb7eba0a0feb3827
ChewMango <55792134+ChewMango@users.noreply.github.com>
2020-05-30 00:35:32 +0100

    Added Audio Option
    In the options menu, there is a volume slider that allows you to adjust the volume of the background music. This music plays through the entirety of the game.

---
+++ CT4005-Assesment2/Assets/Scripts/BackgroundAudio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundAudio : MonoBehaviour
+{
+    private static BackgroundAudio instance = null;
+    public static BackgroundAudio Instance
+    {
+        get { return instance;  }
+    }
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
