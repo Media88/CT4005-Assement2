b76e033003c0a47475dc9272ddf1865c9b5477ea
Ffiffi27 <55792901+Ffiffi27@users.noreply.github.com>
2020-05-31 22:13:45 +0100

    Points system works
    but spawning of the coins are not :)

--- CT4005-Assesment2/Assets/Scripts/ScoreScripts.cs
+++ CT4005-Assesment2/Assets/Scripts/ScoreScripts.cs
@@ -3,14 +3,41 @@ using UnityEngine.UI;
 
 public class ScoreScripts : MonoBehaviour
 {
-    public GameObject scoreText;
-    public int score;
+   public GameObject scoreTextP1;
+   public GameObject scoreTextP2;
+   public GameObject scoreTextP3;
+   public GameObject scoreTextP4;
+   public int scoreP1;
+   public int scoreP2;
+   public int scoreP3;
+   public int scoreP4;
 
-    private void OnTriggerEnter(Collider other)
+  void OnTriggerEnter(Collider other)
     {
-        score++;
-        scoreText.GetComponent<Text>().text = "Score: " + score;
-        Destroy(gameObject);
+    
+       if(other.gameObject.CompareTag ("Player1"))
+       {
+            scoreP1++;
+            scoreTextP1.GetComponent<Text>().text = "Score: " + scoreP1;
+            Destroy(gameObject);
+	    }
+       if(other.gameObject.CompareTag ("Player2"))
+       {
+            scoreP2++;
+            scoreTextP2.GetComponent<Text>().text = "Score: " + scoreP2;
+            Destroy(gameObject);
+       }
+       if(other.gameObject.CompareTag ("Player3"))
+       {
+            scoreP3++;
+            scoreTextP3.GetComponent<Text>().text = "Score: " + scoreP3;
+            Destroy(gameObject);
+	    }
+       if(other.gameObject.CompareTag ("Player4"))
+       {
+            scoreP4++;
+            scoreTextP4.GetComponent<Text>().text = "Score: " + scoreP4;
+            Destroy(gameObject);
+       }
     }
-
 }
