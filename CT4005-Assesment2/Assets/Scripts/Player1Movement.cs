0d28b4c00ad1c30a18f9b558ca9a867594f155b7
Henryabrunning <55792186+Henryabrunning@users.noreply.github.com>
2020-06-01 13:07:54 +0100

    Added basic Run Animation

--- CT4005-Assesment2/Assets/Scripts/Player1Movement.cs
+++ CT4005-Assesment2/Assets/Scripts/Player1Movement.cs
@@ -4,20 +4,36 @@ using UnityEngine;
 
 public class Player1Movement : MonoBehaviour
 {
-   public float speed = 12f;
+    public float speed = 12f;
     public CharacterController controller;
+    
+    public Animator anim;
 
     [SerializeField]
     private string controllerPrefix = "";
 
-  
+    void Start()
+    {
+        //anim = GetComponent<Animator>();
+    }
+
     void Update()
     {
+
         float x = Input.GetAxis(controllerPrefix + "Horizontal");
         float z = Input.GetAxis(controllerPrefix + "Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        if(x != 0 || z != 0)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
     }
 }
