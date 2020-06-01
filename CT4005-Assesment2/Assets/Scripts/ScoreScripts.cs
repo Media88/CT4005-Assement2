9ef22dd582a45d07e092b19abfd80dc44511bdac
Ffiffi27 <55792901+Ffiffi27@users.noreply.github.com>
2020-06-01 17:25:40 +0100

    destroy fixed-ish

--- CT4005-Assesment2/Assets/Scripts/ScoreScripts.cs
+++ CT4005-Assesment2/Assets/Scripts/ScoreScripts.cs
@@ -16,29 +16,29 @@ public class ScoreScripts : MonoBehaviour
 	public void P1Collision()
 	{
 		scoreP1++;
-            scoreTextP1.GetComponent<Text>().text = "Score: " + scoreP1;
-            Destroy(gameObject);
+        scoreTextP1.GetComponent<Text>().text = "Score: " + scoreP1;
+        Destroy(GameObject.FindWithTag("Coin"));
 	}
 
 	public void P2Collision()
 	{
         scoreP2++;
         scoreTextP2.GetComponent<Text>().text = "Score: " + scoreP2;
-        Destroy(gameObject);
+        Destroy(GameObject.FindWithTag("Coin"));
 	}
 
 	public void P3Collision()
 	{
 		scoreP3++;
         scoreTextP3.GetComponent<Text>().text = "Score: " + scoreP3;
-        Destroy(gameObject);
+        Destroy(GameObject.FindWithTag("Coin"));
 	}
 
 	public void P4Collision()
 	{
         scoreP4++;
         scoreTextP4.GetComponent<Text>().text = "Score: " + scoreP4;
-        Destroy(gameObject);
+        Destroy(GameObject.FindWithTag("Coin"));
 	}
 
 }
