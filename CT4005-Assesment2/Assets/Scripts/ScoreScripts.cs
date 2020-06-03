e3b29b22cc80d494d01c44f4f48d84abcd7f44e3
Ffiffi27 <55792901+Ffiffi27@users.noreply.github.com>
2020-06-03 20:28:13 +0100

    fixed coin destroy, for real this time :)

--- CT4005-Assesment2/Assets/Scripts/ScoreScripts.cs
+++ CT4005-Assesment2/Assets/Scripts/ScoreScripts.cs
@@ -18,28 +18,24 @@ public class ScoreScripts : MonoBehaviour
 	{
 		scoreP1++;
         scoreTextP1.GetComponent<Text>().text = "Score: " + scoreP1;
-        Destroy(GameObject.FindWithTag("Coin"));
 	}
 
 	public void P2Collision()
 	{
         scoreP2++;
         scoreTextP2.GetComponent<Text>().text = "Score: " + scoreP2;
-        Destroy(GameObject.FindWithTag("Coin"));
 	}
 
 	public void P3Collision()
 	{
 		scoreP3++;
         scoreTextP3.GetComponent<Text>().text = "Score: " + scoreP3;
-        Destroy(GameObject.FindWithTag("Coin"));
 	}
 
 	public void P4Collision()
 	{
         scoreP4++;
         scoreTextP4.GetComponent<Text>().text = "Score: " + scoreP4;
-        Destroy(GameObject.FindWithTag("Coin"));
 	}
 
 }
