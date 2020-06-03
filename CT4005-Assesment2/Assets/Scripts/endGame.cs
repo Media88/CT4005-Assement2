5b141d4f820951f47289bb524752f0be9a7b7d05
Ffiffi27 <55792901+Ffiffi27@users.noreply.github.com>
2020-06-04 00:21:46 +0100

    Sorry Github had a fit

--- CT4005-Assesment2/Assets/Scripts/endGame.cs
+++ CT4005-Assesment2/Assets/Scripts/endGame.cs
@@ -10,15 +10,37 @@ public class endGame : MonoBehaviour
     [SerializeField]
     private GameObject pauseScreen;
 
+    public ScoreScripts scoreScripts;
+
+    public int scoreP1;
+    public int scoreP2;
+    public int scoreP3;
+    public int scoreP4;
+    public int scoreGoal = 5;
+
     private void Start()
     {
         endScreen.gameObject.SetActive(false);
         pauseScreen.gameObject.SetActive(false);
     }
 
-    private void Update()
+    void Update()
     {
-        
+        if (scoreP1 >= scoreGoal)
+        {
+             endScreen.gameObject.SetActive(true);
+        }
+        if (scoreP2 >= scoreGoal)
+        {
+             endScreen.gameObject.SetActive(true);
+        }
+        if (scoreP3 >= scoreGoal)
+        {
+             endScreen.gameObject.SetActive(true);
+        }
+        if (scoreP4 >= scoreGoal)
+        {
+             endScreen.gameObject.SetActive(true);
+        }
     }
-
 }
