59442ab057e66f9c06e3e4921fdf51d5741190a3
Ffiffi27 <55792901+Ffiffi27@users.noreply.github.com>
2020-06-04 00:14:16 +0100

    Revert "Merged endgamemenu script with endGame Script"
    This reverts commit 1c97965620b92beef44483143b64b8197444fcfe.

--- CT4005-Assesment2/Assets/Scripts/endGame.cs
+++ CT4005-Assesment2/Assets/Scripts/endGame.cs
@@ -10,37 +10,15 @@ public class endGame : MonoBehaviour
     [SerializeField]
     private GameObject pauseScreen;
 
-    public ScoreScripts scoreScripts;
-
-    public int scoreP1;
-    public int scoreP2;
-    public int scoreP3;
-    public int scoreP4;
-    public int scoreGoal = 5;
-
     private void Start()
     {
         endScreen.gameObject.SetActive(false);
         pauseScreen.gameObject.SetActive(false);
     }
 
-    void Update()
+    private void Update()
     {
-        if (scoreP1 >= scoreGoal)
-        {
-             endScreen.gameObject.SetActive(true);
-        }
-        if (scoreP2 >= scoreGoal)
-        {
-             endScreen.gameObject.SetActive(true);
-        }
-        if (scoreP3 >= scoreGoal)
-        {
-             endScreen.gameObject.SetActive(true);
-        }
-        if (scoreP4 >= scoreGoal)
-        {
-             endScreen.gameObject.SetActive(true);
-        }
+        
     }
+
 }
